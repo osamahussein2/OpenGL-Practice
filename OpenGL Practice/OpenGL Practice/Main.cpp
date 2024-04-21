e7256e8aa50be22bfe9bd8dc2e5de46f789eb491
Osama Hussein <osamahusseinn2@gmail.com>
2024-04-21 13:23:18 -0400

    Currently trying to set up an OpenGL window, even though I have a lot of errors

---
+++ OpenGL Practice/OpenGL Practice/Main.cpp
@@ -0,0 +1,18 @@
+/*
+APA references:
+
+	Welcome to OpenGL. (n.d.) Learn OpenGL. 
+		https://learnopengl.com/
+*/
+
+#include <Windows.h>
+#include <glew.h>
+#include <eglew.h>
+#include <wglew.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+int main()
+{
+
+}
