ec8ed0eb74ca292e5923445183998dc9b45c48f7
Osama Hussein <osamahusseinn2@gmail.com>
2024-09-02 21:44:32 -0400

    I implemented the Gouraud shading by setting the light source inside the vertex shader rather than the fragment shader

--- OpenGL Practice/OpenGL Practice/LightingFragmentShader.glsl
+++ OpenGL Practice/OpenGL Practice/LightingFragmentShader.glsl
@@ -1,10 +1,11 @@
 #version 330 core
 
-float ambientLightBrightness;
-vec3 ambientLight;
-vec3 resultingLight;
+/*float ambientLightBrightness;
+vec3 ambientLight;*/
 
-vec3 normalizeNormals;
+in vec3 resultingLight;
+
+/*vec3 normalizeNormals;
 vec3 lightDirection;
 vec3 diffuseLight;
 
@@ -12,44 +13,44 @@ float specularBrightness;
 vec3 viewDirection;
 vec3 reflectionDirection;
 float specularValue;
-vec3 specularLight;
+vec3 specularLight;*/
 
 out vec4 fragColor;
 
 in vec3 FragPosition;
 in vec3 Normal;
 
-uniform vec3 objectColor;
+/*uniform vec3 objectColor;
 uniform vec3 lightColor;
 
 uniform vec3 lightPosition;
-uniform vec3 viewPosition;
+uniform vec3 viewPosition;*/
 
 void main()
 {
 	// Ambient lighting
-	ambientLightBrightness = 0.1;
-	ambientLight = ambientLightBrightness * lightColor;
+	//ambientLightBrightness = 0.1;
+	//ambientLight = ambientLightBrightness * lightColor;
 
 	// Diffuse lighting
 
 	// Make sure these newly defined vec3s are unit vectors, hence why they should be normalized
-	normalizeNormals = normalize(Normal);
+	//normalizeNormals = normalize(Normal);
 
 	// We know the light direction is equal to the light's position minus the fragment's position
-	lightDirection = normalize(lightPosition - FragPosition);
+	/*lightDirection = normalize(lightPosition - FragPosition);
 
 	diffuseLight = max(dot(normalizeNormals, lightDirection), 0.0) * lightColor;
 
 	specularBrightness = 0.5;
 
-	viewDirection = normalize(viewPosition - FragPosition);
+	viewDirection = normalize(viewPosition - FragPosition);*/
 
 	/* We have to negate the light direction by adding a minus sign beside it because the light direction 
 	vector is currently looking the other way around, from the fragment position towards the light source. */
 
 	// The reflect function expects a direction vector and a normal vector as its 2 arguments
-	reflectionDirection = reflect(-lightDirection, normalizeNormals);
+	//reflectionDirection = reflect(-lightDirection, normalizeNormals);
 
 	/* We first calculate the dot produce between the view direction and the reflection direction (but also,
 	make sure the the value isn't negative) and then raise it to the power of 32. */
@@ -58,9 +59,10 @@ void main()
 	the more it properly reflects the light instead of scattering it all around, meaning that the highlight becomes
 	smaller. */
 
-	specularValue = pow(max(dot(viewDirection, reflectionDirection), 0.0), 32);
-	specularLight = specularBrightness * specularValue * lightColor;
+	/*specularValue = pow(max(dot(viewDirection, reflectionDirection), 0.0), 32);
+	specularLight = specularBrightness * specularValue * lightColor;*/
 
-	resultingLight = (ambientLight + diffuseLight + specularLight) * objectColor;
+	//resultingLight = (ambientLight + diffuseLight + specularLight) * objectColor;
+
 	fragColor = vec4(resultingLight, 1.0);
 }
--- OpenGL Practice/OpenGL Practice/LightingVertexShader.glsl
+++ OpenGL Practice/OpenGL Practice/LightingVertexShader.glsl
@@ -3,6 +3,8 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 
+out vec3 resultingLight;
+
 // Send the fragment position and the normal to the fragment shader so that we can set them there
 out vec3 FragPosition;
 out vec3 Normal;
@@ -11,14 +13,44 @@ uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+
+uniform vec3 lightPosition;
+uniform vec3 viewPosition;
+
 void main()
 {
 	gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
 	// Multiply the fragment position using the model matrix to transform it to world space coordinates
-	FragPosition = vec3(viewMatrix * modelMatrix * vec4(position, 1.0));
+	FragPosition = vec3(modelMatrix * vec4(position, 1.0));
 
 	// Generate the normal matrix by using the transpose and inverse functions (only if the vec3 is non-uniform scale)
 	// The matrix has to be casted as a mat3 to make sure it doesn't have translation properties anymore
-	Normal =  mat3(transpose(inverse(viewMatrix * modelMatrix))) * normal;
+	Normal =  mat3(transpose(inverse(modelMatrix))) * normal;
+
+	/* The lighting looks a bit weird when I implemented the Gouraud shading because the lighting shader applies
+	to every single vertice that is being instantiated into the vertex shader and the lighting won't be as realistic
+	compared to the fragment shader where the lighting will look a lot more realistic. */
+
+	// Ambient lighting
+	float ambientLightBrightness = 0.1;
+	vec3 ambientLight = ambientLightBrightness * lightColor;
+
+	// Diffuse lighting
+	vec3 normalizeNormals = normalize(Normal);
+	vec3 lightDirection = normalize(lightPosition - FragPosition);
+
+	vec3 diffuseLight = max(dot(normalizeNormals, lightDirection), 0.0) * lightColor;
+
+	float specularBrightness = 0.5;
+
+	vec3 viewDirection = normalize(viewPosition - FragPosition);
+	vec3 reflectionDirection = reflect(-lightDirection, normalizeNormals);
+
+	float specularValue = pow(max(dot(viewDirection, reflectionDirection), 0.0), 32);
+	vec3 specularLight = specularBrightness * specularValue * lightColor;
+
+	resultingLight = (ambientLight + diffuseLight + specularLight) * objectColor;
 }
